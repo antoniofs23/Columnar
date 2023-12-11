12b0e57f3b0f12cfb8f8f267c91874e96a62d576
Antonio Fernandez <af3036@nyu.edu>
2023-12-10 21:04:54 -0500

    Update to do list

--- winManage.sh
+++ winManage.sh
@@ -13,6 +13,8 @@
 #
 #    If a window is closed or another is open you can re-tile
 #    by pressing F7
+#    To do tomorrow: Fix how the code handles one window
+#    Add 1 large window and the others are smaller
 
 xinput test-xi2 --root 3 | grep -A2 --line-buffered RawKeyRelease | while read -r line;
 do

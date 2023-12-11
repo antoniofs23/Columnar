95b3c12c48cb6220c322426c7e5b355dd1249a93
antoniofs23 <af3036@nyu.edu>
2023-12-11 12:39:14 -0500

    fixed doc string

--- winManage.sh
+++ winManage.sh
@@ -13,8 +13,6 @@
 #
 #    If a window is closed or another is open you can re-tile
 #    by pressing F7
-#    To do tomorrow: Fix how the code handles one window
-#    Add 1 large window and the others are smaller
 
 xinput test-xi2 --root 3 | grep -A2 --line-buffered RawKeyRelease | while read -r line;
 do
@@ -87,9 +85,6 @@ do
                 x_avg=$(($total_x/$winTiles))
                 w_avg=$(($total_w/$winTiles))
 
-                #echo $x_avg
-                #echo $w_avg
-                #printf '%s\n' "${w_padding[@]}"
                 # get monitor resolution -- first col
                 resolution=$(xrandr | grep "*" | awk '{print $1}')
 

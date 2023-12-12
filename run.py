d833214a7178e46f6eff384ec647e7335e369f48
antoniofs23 <af3036@nyu.edu>
2023-12-12 14:30:38 -0500

    altered app logo

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ APPINDICATOR_ID = 'myappindicator'
 
 
 def main():
-    indicator = appindicator.Indicator.new(APPINDICATOR_ID, os.path.abspath('sample_icon.svg'), appindicator.IndicatorCategory.SYSTEM_SERVICES)
+    indicator = appindicator.Indicator.new(APPINDICATOR_ID, os.path.abspath('columnar.svg'), appindicator.IndicatorCategory.SYSTEM_SERVICES)
     indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
     indicator.set_menu(build_menu())
     notify.init(APPINDICATOR_ID)
